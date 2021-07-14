8d25fccfa1dbffd2174a32a75e19bca27990b086
Elcajon <github@max-venz.de>
2021-07-14 15:53:53 +0200

    Update Nextcloud Doc & Translation

--- nextcloud/rootfs/defaults/autoconfig.php
+++ nextcloud/rootfs/defaults/autoconfig.php
@@ -5,6 +5,7 @@ $AUTOCONFIG['dbname'] = 'nextcloud';
 $AUTOCONFIG['dbuser'] = 'HADBUSER';
 $AUTOCONFIG['dbpass'] = 'HADBPASS';
 $AUTOCONFIG['dbhost'] = 'HADBHOST';
+$AUTOCONFIG['dbport'] = 'HADBPORT';
 $AUTOCONFIG['directory'] = '/data/nextcloud_data';
 
 $autoconfig_enabled = true;
