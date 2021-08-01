0a52d11a187f026f509ae5062f1ecea8fbc83696
Elcajon <github@max-venz.de>
2021-08-01 17:40:49 +0200

    Change default directory of Nextcloud

--- nextcloud/rootfs/defaults/autoconfig.php
+++ nextcloud/rootfs/defaults/autoconfig.php
@@ -6,6 +6,6 @@ $AUTOCONFIG['dbuser'] = 'HADBUSER';
 $AUTOCONFIG['dbpass'] = 'HADBPASS';
 $AUTOCONFIG['dbhost'] = 'HADBHOST';
 $AUTOCONFIG['dbport'] = 'HADBPORT';
-$AUTOCONFIG['directory'] = '/data/nextcloud_data';
+$AUTOCONFIG['directory'] = '/data/data';
 
 $autoconfig_enabled = true;
